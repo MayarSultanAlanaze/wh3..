5348cc39e1d8483193a75999441dba4735a2b121
MayarSultanAlanaze <126346292+MayarSultanAlanaze@users.noreply.github.com>
2023-05-10 20:24:15 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,122 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    {
+        {
+            {
+                {
+                    {
+                        {
+                            {
+                                {
+                                    {
+
+                                    }
+         /* 1 question
+        int num1=125;
+        int num2=24;
+        int sum=num1+num2;
+        int sub=num1-num2;
+        int mul=num1*num2;
+        int div=num1/num2;
+        int result=num1%num2;
+        System.out.println(" 125 + 24 = " +sum);
+        System.out.println(" 125 - 24 = " +sub);
+        System.out.println(" 125 * 24 = " +mul);
+        System.out.println(" 125 / 24 = " +div);
+        System.out.println("125 mod 24 =" + result);*/
+                                }/* 2 question
+        String text="THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.";
+        System.out.println(text.toLowerCase()); */
+                            }/* 3 question
+        String txet="Java Bootcamp";
+        System.out.printf(txet);
+        int index1=txet.charAt(0);
+        int index2=txet.charAt(10);
+        System.out.println((char)index1);
+        System.out.println((char)index2); */
+                        }/* 4 question
+         Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0"); */
+                    } /* 5 question
+         Scanner S=new Scanner(System.in);
+         System.out.println("please enter your role");
+         String role=S.nextLine();
+         if (role.equals("admin")){
+         System.out.println("welcome admin");
+         } else if (role.equals("superuser")) {
+            System.out.println("welcome superuser");
+         } else if (role.equals("user")) {
+            System.out.println("welcome user");*/
+                }/* 6 question
+          Scanner s = new Scanner(System.in);
+          System.out.println("please enter first number");
+          int num1 = s.nextInt();
+          System.out.println("please enter second number");
+          int num2 = s.nextInt();
+          System.out.println("please enter third number");
+          int num3 = s.nextInt();
+          System.out.println("The result is:" + sumoftwo(num1, num2, num3));
+          }
+                }/* 7 question
+          Scanner s = new Scanner(System.in);
+          System.out.println("please enter first number");
+          int num1 = s.nextInt();
+          System.out.println("please enter second number");
+          int num2 = s.nextInt();
+          System.out.println("please enter third number");
+          int num3 = s.nextInt();
+
+          if (num1>num2)
+          if (num1>num3)
+          System.out.println("The greatest:" +num1);
+          if (num2>num1)
+          if (num2>num3)
+          System.out.println("The greatest:" +num2);
+          if (num3>num1)
+          if (num3>num2)
+          System.out.println("The greatest:" +num3);*/
+            }/* 8 question
+          Scanner s = new Scanner(System.in);
+          System.out.println("please enter number:");
+          int day = s.nextInt();
+          System.out.println(getDayName(day));
+          }
+
+          public static String getDayName(int day) {
+          String dayName = "";
+          switch (day) {
+          case 1:
+          System.out.println(dayName = "Monday");
+          break;
+          case 2:
+          System.out.println(dayName = "Tuesday");
+          break;
+          case 3:
+          System.out.println(dayName = "Wednesday");
+          break;
+          case 4:
+          System.out.println(dayName = "Thursday");
+          break;
+          case 5:
+          System.out.println(dayName = "Friday");
+          break;
+          case 6:
+          System.out.println(dayName = "Saturday");
+          break;
+          case 7:
+          System.out.println(dayName = "Sunday");
+           break;
+           default:
+           System.out.println(dayName = "Invalid day range");
+
+           return dayName;
+           }
+           return dayName;*/
+        }
+    }
+}
